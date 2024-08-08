39368df4e5989515b1d5345fd17d22bb562a2fec
冬喃 <xuenhao.xeh@antgroup.com>
2024-08-08 17:49:34 +0800

    monitor, slo and inspection build

--- common/prometheus/prometheus_test.go
+++ common/prometheus/prometheus_test.go
@@ -4,10 +4,12 @@ import (
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"testing"
+	"time"
 )
 
 func TestStartPrometheusListen(t *testing.T) {
 	go StartPrometheusListen(10080)
+	time.Sleep(1 * time.Second)
 	_, err := http.Get("http://localhost:10080/metrics")
 	assert.Nil(t, err)
 }

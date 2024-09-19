ab26421ced8338178a806c6afee1536c1547870c
冬喃 <xuenhao.xeh@antgroup.com>
2024-09-19 10:41:48 +0800

    fix update status cause metadata change

--- virtual_kubelet/pod.go
+++ virtual_kubelet/pod.go
@@ -184,8 +184,8 @@ func (pc *PodController) updatePodStatus(ctx context.Context, podFromKubernetes 
 		}
 	}
 
-	podFromProvider.ResourceVersion = "0"
-	if err := pc.client.Status().Update(ctx, podFromProvider); err != nil && !errors.IsNotFound(err) {
+	podFromKubernetes.Status = podFromProvider.Status
+	if err := pc.client.Status().Update(ctx, podFromKubernetes); err != nil && !errors.IsNotFound(err) {
 		span.SetStatus(err)
 		return pkgerrors.Wrap(err, "error while updating pod status in kubernetes")
 	}
